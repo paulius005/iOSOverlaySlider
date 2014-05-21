56336fc88d3ee24753034cff074a4f0db9f0728c
paulius005 <pdragunas@gmail.com>
2014-05-21 00:07:33 -0500

    first commit

---
+++ PDOverlaySlider.m
@@ -0,0 +1,31 @@
+//
+//  PDOverlaySlider.m
+//  OverlaySlider
+//
+//  Created by X on 5/21/14.
+//  Copyright (c) 2014 Paulius Darguans. All rights reserved.
+//
+
+#import "PDOverlaySlider.h"
+
+@implementation PDOverlaySlider
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
